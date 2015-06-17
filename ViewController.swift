18dd0653bc4cf0f728982ab7f1330e202049f7b5
antflash <zp1030@gmail.com>
2015-06-17 14:33:55 +0800

    数据模型建立

--- ViewController.swift
+++ ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var cd:KeleData = KeleData()
+    
+    @IBOutlet weak var nextBtn: UIButton!
+    @IBOutlet weak var preBtn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //println(cd.monthDateRange(NSDate()))
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onNextPressed(sender: AnyObject) {
+        cd.next()
+        
+    }
+    @IBAction func onPrePressed(sender: AnyObject) {
+        
+        cd.pre()
+    }
 
 }
 
